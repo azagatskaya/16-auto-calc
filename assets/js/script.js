c1ee7ed8b1426476b8627749c2c138e7788c80a9
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-04-08 21:33:11 +0300

    220408_2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,45 @@
 let makeAndModels = [
 	["mitsubishi",
-		["Lancer", 200000],
-		["Outlander", 250000],
-		["Space Star", 150000]],
+		["Lancer", 500000],
+		["Outlander", 600000],
+		["Space Star", 400000]],
 	["suzuki",
-		["Grand Vitara", 200000],
-		["SX4", 150000],
-		["Swift", 100000]],
+		["Grand Vitara", 550000],
+		["SX4", 500000],
+		["Swift", 450000]],
 	["toyota",
-		["Avensis", 300000],
-		["Corolla", 200000],
-		["Yaris", 150000]]
+		["Avensis", 600000],
+		["Corolla", 500000],
+		["Yaris", 450000]]
 ];
 
 let make = getMake();
 let model = getModel();
 
 make.addEventListener('change', function () {
-	// isChooseExists(".select--make", "-Марка-");
-	removeOptions(".select--model");
-	let index = getMakeIndex(getMake());
+	removeOptions('.select--model');
+	let index = getMakeIndex(make);
 	addModels(index);
 	
-	let fieldset = document.querySelector(".select--model");
-	fieldset.removeAttribute("disabled");
+	let fieldset = document.querySelector('.select--model');
+	fieldset.removeAttribute('disabled');
 });
 
 model.addEventListener('change', function () {
-	let fieldset = document.querySelector(".select--year");
-	fieldset.removeAttribute("disabled");
+	let fieldset = document.querySelector('.select--year');
+	fieldset.removeAttribute('disabled');
 });
 
 document.querySelector('.calculator__button').addEventListener('click', calculatePrice);
 
 function getMake(){
-	return document.querySelector(".select--make");
+	return document.querySelector('.select--make');
 }
 function getModel() {
-	return document.querySelector(".select--model");
+	return document.querySelector('.select--model');
 }
 function getYearValue() {
-	return document.querySelector(".select--year").value;
+	return document.querySelector('.select--year').value;
 }
 function getMakeIndex(make) {
 	for (let i = 0; i < makeAndModels.length; i++){
@@ -53,9 +52,6 @@ function getMakeIndex(make) {
 
 function getModelIndex(make, model) {
 	let makeIndex = getMakeIndex(make);
-	console.log(makeIndex);
-	console.log(makeAndModels[makeIndex].length);
-	console.log(model.value);
 	for (let i = 1; i < makeAndModels[makeIndex].length; i++){
 		if (makeAndModels[makeIndex][i][0] == model.value) {
 			return i;
@@ -68,36 +64,96 @@ function addModels(makeIndex) {
 	for (let i = 1; i < makeAndModels[makeIndex].length; i++){
 		let newOption = document.createElement('option');
 		newOption.innerHTML = makeAndModels[makeIndex][i][0];
-		document.querySelector(".select--model").append(newOption); 
+		document.querySelector('.select--model').append(newOption); 
 	}
 }
 
 function removeOptions(selectClass) {
-	let options = document.querySelectorAll(selectClass + " option");
+	let options = document.querySelectorAll(selectClass + ' option');
 	if (options.length > 1) {
 		for (let i = 1; i < options.length; i++){
 			options[i].remove();
 		}
 	}
 }
-function isChooseExists(selectClass, label) {
-	let chooseOption = document.querySelector(selectClass + " option");
-	if (chooseOption.label == label) {
-		chooseOption.remove();
-	}
-}
 
 function calculatePrice() {
-	getBasePrice(); 
-	getAmortizationDiscount();
+	let carPrice = getBasePrice() + getAmortizationDiscount() +
+		getFuelPrice() + getDriveUnitPrice() + getTransmissionPrice() +
+		getCarBodyPrice() + getEngineVolumePrice() + getHorsePowerPrice() + 
+		getMileagePrice() + getEquipmentPrice();
+	document.querySelector('.result-window').innerHTML = carPrice + ' руб.';
 }
 function getBasePrice() {
 	let makeIndex = getMakeIndex(make);
 	let modelIndex = getModelIndex(make, model);
-	let basePrice = makeAndModels[makeIndex][modelIndex][1];
-	console.log(basePrice);
+	return makeAndModels[makeIndex][modelIndex][1];
 }
 function getAmortizationDiscount() {
-	let year = getYearValue();
-	console.log(year);
+	let selectedYear = getYearValue();
+	let currentYear = new Date().getFullYear();
+	let carAge = currentYear - selectedYear;
+	return (carAge > 5) ? (-carAge *7000) : ((5 - carAge) * 15000);
+}
+function getFuel() {
+	return document.querySelector('.radio--fuel input:checked').id;
+}
+function getFuelPrice(){
+	let fuel = getFuel();
+	if (fuel == 'hybrid') { return 20000; }
+	if (fuel == 'diesel') { return -20000; }	
+	if (fuel == 'petrol') { return 0; }	
+}
+function getDriveUnit() {
+	return document.querySelector('.radio--drive-unit input:checked').id;
+}
+function getDriveUnitPrice() {
+	let dUnit = getDriveUnit();
+	if (dUnit == 'front-du') { return 0; }
+	if (dUnit == 'full-du') { return 40000; }	
+}
+function getTransmission() {
+	return document.querySelector('.radio--transmission input:checked').id;
+}
+function getTransmissionPrice() {
+	let transmission = getTransmission();
+	if (transmission == 'automatic-tr') { return 50000; }
+	if (transmission == 'mechanic-tr') { return 0; }	
+	if (transmission == 'robotic-tr') { return 30000; }	
+}
+function getCarBody() {
+	return document.querySelector('.radio--car-body input:checked').id;
+}
+function getCarBodyPrice() {
+	let carBody = getCarBody();
+	if (carBody == 'sedan') { return 30000; }
+	if (carBody == 'hatchback') { return 0; }	
+	if (carBody == 'universal') { return 60000; }	
+}
+
+function getEngineVolume() {
+	return document.querySelector('.input--engine-volume').value;
+}
+function getEngineVolumePrice() {
+	let engineVol = getEngineVolume();
+	return engineVol * 20;
+}
+
+function getHorsePower() {
+	return document.querySelector('.input--horse-power').value;
+}
+function getHorsePowerPrice() {
+	let horsePower = getHorsePower();
+	return horsePower * 1000;
+}
+
+function getMileage() {
+	return document.querySelector('.input--mileage').value;
+}
+function getMileagePrice() {
+	return -getMileage() * 0.7;
+}
+function getEquipmentPrice() {
+	return document.querySelectorAll('.equipment input:checked').length * 10000;
+
 }
